342f305f209d7a7b5c2291f93b4402c22c9dc585
AlecsHome <alecshome.ah@gmail.com>
2025-11-07 14:31:19 +0300

    Update clock.h

--- software/firmware/clock.h
+++ software/firmware/clock.h
@@ -3,11 +3,11 @@
 #define	__clock_h_included__
 
 #ifndef F_CPU /* should be defined in Makefile */
-#define F_CPU           16000000L   /* 16MHz */
+#define F_CPU           16000000UL   /* 16MHz */
 #endif
 
 #define TIMERVALUE      TCNT0
-#define CLOCK_T_320us	80 // лучше подсчитать сразу вместо: (320e-6*F_CPU/64) /* prescaler is 64 as defined below */
+#define CLOCK_T_320us	 (320e-6*F_CPU/64) /* prescaler is 64 as defined below */
 
 
 #ifdef __AVR_ATmega8__
@@ -21,4 +21,5 @@
 void clockWait(uint8_t time);
 
 #endif /* __clock_h_included__ */
+
 
