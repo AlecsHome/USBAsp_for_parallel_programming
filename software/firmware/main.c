9f4b8cb7fa7971dba7f6cfefd05a44ca78cd0a6c
AlecsHome <alecshome.ah@gmail.com>
2025-11-07 15:29:14 +0300

    Update main.c
    Обновляем: объявление структуры, usbFunctionSetup для использования структуры, usbFunctionRead и usbFunctionWrite

--- software/firmware/main.c
+++ software/firmware/main.c
@@ -29,346 +29,260 @@
 #include "tpi.h"
 #include "tpi_defs.h"
 
+// В начале main.c, после включения заголовочных файлов
+typedef struct {
+    uchar state;
+    uchar sck_option;
+    uchar address_newmode;
+    unsigned long address;
+    unsigned int nbytes;
+    unsigned int pagesize;
+    uchar blockflags;
+    uchar pagecounter;
+} ProgrammingState;
+
+static ProgrammingState prog = {
+    .state = PROG_STATE_IDLE,
+    .sck_option = USBASP_ISP_SCK_AUTO,
+    .address_newmode = 0,
+    .address = 0,
+    .nbytes = 0,
+    .pagesize = 0,
+    .blockflags = 0,
+    .pagecounter = 0
+};
+
+// Обновляем extern объявления для isp.c
+extern unsigned int prog_pagesize;
+extern uchar prog_pagecounter;
+
 static uchar replyBuffer[8];
 
-static uchar prog_state = PROG_STATE_IDLE;
-static uchar prog_sck = USBASP_ISP_SCK_AUTO;
+//static uchar prog_state = PROG_STATE_IDLE;
+//static uchar prog_sck = USBASP_ISP_SCK_AUTO;
 
-static uchar prog_address_newmode = 0;
-static unsigned long prog_address;
-static unsigned int prog_nbytes = 0;
-unsigned int prog_pagesize;
-static uchar prog_blockflags;
-uchar prog_pagecounter;
+//static uchar prog_address_newmode = 0;
+//static unsigned long prog_address;
+//static unsigned int prog_nbytes = 0;
+//unsigned int prog_pagesize;
+//static uchar prog_blockflags;
+//uchar prog_pagecounter;
 
 uchar usbFunctionSetup(uchar data[8]) {
+    uchar len = 0;
 
-	uchar len = 0;
+    if (data[1] == USBASP_FUNC_CONNECT) {
+        /* set SCK speed */
+        if ((SLOW_SCK_PIN & (1 << SLOW_SCK_NUM)) == 0) {
+            ispSetSCKOption(USBASP_ISP_SCK_8);
+        } else {
+            ispSetSCKOption(prog.sck_option);  // Используем структуру
+        }
 
-	if (data[1] == USBASP_FUNC_CONNECT) {
+        /* set compatibility mode of address delivering */
+        prog.address_newmode = 0;  // Используем структуру
 
-		/* set SCK speed */
-		if ((SLOW_SCK_PIN & (1 << SLOW_SCK_NUM)) == 0) {
-			ispSetSCKOption(USBASP_ISP_SCK_8);
-		} else {
-			ispSetSCKOption(prog_sck);
-		}
+        ledRedOn();
+        ispConnect();
 
-		/* set compatibility mode of address delivering */
-		prog_address_newmode = 0;
+    } else if (data[1] == USBASP_FUNC_DISCONNECT) {
+        ispDisconnect();
+        ledRedOff();
 
-		ledRedOn();
-		ispConnect();
+    } else if (data[1] == USBASP_FUNC_READFLASH) {
+        if (!prog.address_newmode)  // Используем структуру
+            prog.address = (data[3] << 8) | data[2];  // Используем структуру
 
-	} else if (data[1] == USBASP_FUNC_DISCONNECT) {
-		ispDisconnect();
-		ledRedOff();
+        prog.nbytes = (data[7] << 8) | data[6];  // Используем структуру
+        prog.state = PROG_STATE_READFLASH;  // Используем структуру
+        len = 0xff;
 
-	} else if (data[1] == USBASP_FUNC_TRANSMIT) {
-		switch(data[2])
-		{
-			case 0x30: //Get ID
-			{
-				replyBuffer[0] = 0x00;
-				replyBuffer[1] = 0x30;
-				replyBuffer[2] = 0x00;
-				replyBuffer[3] = avr_getId(data[4] & 0x3);
-				break;
-			}
-			case 0x50: //Read Low/Ext Fuse
-			{
-				if(data[3] == 0x08)//Ext fuse
-				{
-					replyBuffer[0] = 0x00;
-					replyBuffer[1] = 0x50;
-					replyBuffer[2] = 0x00;
-					replyBuffer[3] = avr_getFuse(0x02);
-					break;
-				}
-				else //Low fuse
-				{
-					replyBuffer[0] = 0x00;
-					replyBuffer[1] = 0x50;
-					replyBuffer[2] = 0x00;
-					replyBuffer[3] = avr_getFuse(0x01);
-					break;
-				}
-			}
-			case 0x58: //Read High Fuse/Lockbits
-			{
-				if(data[3] == 0x08)//High fuse
-				{
-					replyBuffer[0] = 0x00;
-					replyBuffer[1] = 0x58;
-					replyBuffer[2] = 0x00;
-					replyBuffer[3] = avr_getFuse(0x00);
-					break;
-				}
-				else //Lock bits
-				{
-					replyBuffer[0] = 0x00;
-					replyBuffer[1] = 0x58;
-					replyBuffer[2] = 0x00;
-					replyBuffer[3] = avr_getFuse(0x03);
-					break;
-				}
-			}
-			case 0xAC: //Write All fuse/Lock bits/Erase/Prog Enable
-			{
-				replyBuffer[0] = 0x00;
-				replyBuffer[1] = 0xAC;
-				replyBuffer[2] = data[3];
-				replyBuffer[3] = data[4];
-				if(data[3] == 0x53) break; //Prog Enable
-				else if(data[3] == 0x80) avr_erase(); //Erase
-				else avrSetFuse(data[3], data[5]);
-				break;
-			}
-			default:
-			{
-				replyBuffer[0] = 0;
-				replyBuffer[1] = 0;
-				replyBuffer[2] = 0;
-				replyBuffer[3] = 0;
-			}
-		}
+    } else if (data[1] == USBASP_FUNC_READEEPROM) {
+        if (!prog.address_newmode)  // Используем структуру
+            prog.address = (data[3] << 8) | data[2];  // Используем структуру
 
+        prog.nbytes = (data[7] << 8) | data[6];  // Используем структуру
+        prog.state = PROG_STATE_READEEPROM;  // Используем структуру
+        len = 0xff;
 
+    } else if (data[1] == USBASP_FUNC_ENABLEPROG) {
+        replyBuffer[0] = ispEnterProgrammingMode();
+        len = 1;
 
+    } else if (data[1] == USBASP_FUNC_WRITEFLASH) {
+        if (!prog.address_newmode)  // Используем структуру
+            prog.address = (data[3] << 8) | data[2];  // Используем структуру
 
-		len = 4;
+        prog.pagesize = data[4];  // Используем структуру
+        prog.blockflags = data[5] & 0x0F;  // Используем структуру
+        prog.pagesize += (((unsigned int) data[5] & 0xF0) << 4);  // Используем структуру
+        
+        if (prog.blockflags & PROG_BLOCKFLAG_FIRST) {  // Используем структуру
+            prog.pagecounter = prog.pagesize;  // Используем структуру
+        }
+        
+        prog.nbytes = (data[7] << 8) | data[6];  // Используем структуру
+        prog.state = PROG_STATE_WRITEFLASH;  // Используем структуру
+        len = 0xff;
 
-	} else if (data[1] == USBASP_FUNC_READFLASH) {
+    } else if (data[1] == USBASP_FUNC_WRITEEEPROM) {
+        if (!prog.address_newmode)  // Используем структуру
+            prog.address = (data[3] << 8) | data[2];  // Используем структуру
 
-		if (!prog_address_newmode)
-			prog_address = (data[3] << 8) | data[2];
+        prog.pagesize = 0;  // Используем структуру
+        prog.blockflags = 0;  // Используем структуру
+        prog.nbytes = (data[7] << 8) | data[6];  // Используем структуру
+        prog.state = PROG_STATE_WRITEEEPROM;  // Используем структуру
+        len = 0xff;
 
-		prog_nbytes = (data[7] << 8) | data[6];
-		prog_state = PROG_STATE_READFLASH;
-		len = 0xff; /* multiple in */
+    } else if (data[1] == USBASP_FUNC_SETLONGADDRESS) {
+        prog.address_newmode = 1;  // Используем структуру
+        prog.address = *((unsigned long*) &data[2]);  // Используем структуру
 
-	} else if (data[1] == USBASP_FUNC_READEEPROM) {
+    } else if (data[1] == USBASP_FUNC_SETISPSCK) {
+        prog.sck_option = data[2];  // Используем структуру
+        replyBuffer[0] = 0;
+        len = 1;
 
-		if (!prog_address_newmode)
-			prog_address = (data[3] << 8) | data[2];
+    } else if (data[1] == USBASP_FUNC_TPI_CONNECT) {
+        tpi_dly_cnt = data[2] | (data[3] << 8);
+        ISP_OUT |= (1 << ISP_RST);
+        ISP_DDR |= (1 << ISP_RST);
+        clockWait(3);
+        ISP_OUT &= ~(1 << ISP_RST);
+        ledRedOn();
+        clockWait(16);
+        tpi_init();
 
-		prog_nbytes = (data[7] << 8) | data[6];
-		prog_state = PROG_STATE_READEEPROM;
-		len = 0xff; /* multiple in */
+    } else if (data[1] == USBASP_FUNC_TPI_DISCONNECT) {
+        tpi_send_byte(TPI_OP_SSTCS(TPISR));
+        tpi_send_byte(0);
+        clockWait(10);
+        ISP_OUT |= (1 << ISP_RST);
+        clockWait(5);
+        ISP_OUT &= ~(1 << ISP_RST);
+        clockWait(5);
+        ISP_DDR &= ~((1 << ISP_RST) | (1 << ISP_SCK) | (1 << ISP_MOSI));
+        ISP_OUT &= ~((1 << ISP_RST) | (1 << ISP_SCK) | (1 << ISP_MOSI));
+        ledRedOff();
 
-	} else if (data[1] == USBASP_FUNC_ENABLEPROG) {
-		replyBuffer[0] = ispEnterProgrammingMode();
-		len = 1;
+    } else if (data[1] == USBASP_FUNC_TPI_READBLOCK) {
+        prog.address = (data[3] << 8) | data[2];  // Используем структуру
+        prog.nbytes = (data[7] << 8) | data[6];  // Используем структуру
+        prog.state = PROG_STATE_TPI_READ;  // Используем структуру
+        len = 0xff;
 
-	} else if (data[1] == USBASP_FUNC_WRITEFLASH) {
+    } else if (data[1] == USBASP_FUNC_TPI_WRITEBLOCK) {
+        prog.address = (data[3] << 8) | data[2];  // Используем структуру
+        prog.nbytes = (data[7] << 8) | data[6];  // Используем структуру
+        prog.state = PROG_STATE_TPI_WRITE;  // Используем структуру
+        len = 0xff;
 
-		if (!prog_address_newmode)
-			prog_address = (data[3] << 8) | data[2];
+    } else if (data[1] == USBASP_FUNC_GETCAPABILITIES) {
+        replyBuffer[0] = USBASP_CAP_0_TPI;
+        replyBuffer[1] = 0;
+        replyBuffer[2] = 0;
+        replyBuffer[3] = 0;
+        len = 4;
+    }
 
-		prog_pagesize = data[4];
-		prog_blockflags = data[5] & 0x0F;
-		prog_pagesize += (((unsigned int) data[5] & 0xF0) << 4);
-		if (prog_blockflags & PROG_BLOCKFLAG_FIRST) {
-			prog_pagecounter = prog_pagesize;
-		}
-		prog_nbytes = (data[7] << 8) | data[6];
-		prog_state = PROG_STATE_WRITEFLASH;
-		len = 0xff; /* multiple out */
-
-	} else if (data[1] == USBASP_FUNC_WRITEEEPROM) {
-
-		if (!prog_address_newmode)
-			prog_address = (data[3] << 8) | data[2];
-
-		prog_pagesize = 0;
-		prog_blockflags = 0;
-		prog_nbytes = (data[7] << 8) | data[6];
-		prog_state = PROG_STATE_WRITEEEPROM;
-		len = 0xff; /* multiple out */
-
-	} else if (data[1] == USBASP_FUNC_SETLONGADDRESS) {
-
-		/* set new mode of address delivering (ignore address delivered in commands) */
-		prog_address_newmode = 1;
-		/* set new address */
-		prog_address = *((unsigned long*) &data[2]);
-
-	} else if (data[1] == USBASP_FUNC_SETISPSCK) {
-
-		/* set sck option */
-		prog_sck = data[2];
-		replyBuffer[0] = 0;
-		len = 1;
-
-	} else if (data[1] == USBASP_FUNC_TPI_CONNECT) {
-		tpi_dly_cnt = data[2] | (data[3] << 8);
-
-		/* RST high */
-		ISP_OUT |= (1 << ISP_RST);
-		ISP_DDR |= (1 << ISP_RST);
-
-		clockWait(3);
-
-		/* RST low */
-		ISP_OUT &= ~(1 << ISP_RST);
-		ledRedOn();
-
-		clockWait(16);
-		tpi_init();
-
-	} else if (data[1] == USBASP_FUNC_TPI_DISCONNECT) {
-
-		tpi_send_byte(TPI_OP_SSTCS(TPISR));
-		tpi_send_byte(0);
-
-		clockWait(10);
-
-		/* pulse RST */
-		ISP_OUT |= (1 << ISP_RST);
-		clockWait(5);
-		ISP_OUT &= ~(1 << ISP_RST);
-		clockWait(5);
-
-		/* set all ISP pins inputs */
-		ISP_DDR &= ~((1 << ISP_RST) | (1 << ISP_SCK) | (1 << ISP_MOSI));
-		/* switch pullups off */
-		ISP_OUT &= ~((1 << ISP_RST) | (1 << ISP_SCK) | (1 << ISP_MOSI));
-
-		ledRedOff();
-
-	} else if (data[1] == USBASP_FUNC_TPI_RAWREAD) {
-		replyBuffer[0] = tpi_recv_byte();
-		len = 1;
-
-	} else if (data[1] == USBASP_FUNC_TPI_RAWWRITE) {
-		tpi_send_byte(data[2]);
-
-	} else if (data[1] == USBASP_FUNC_TPI_READBLOCK) {
-		prog_address = (data[3] << 8) | data[2];
-		prog_nbytes = (data[7] << 8) | data[6];
-		prog_state = PROG_STATE_TPI_READ;
-		len = 0xff; /* multiple in */
-
-	} else if (data[1] == USBASP_FUNC_TPI_WRITEBLOCK) {
-		prog_address = (data[3] << 8) | data[2];
-		prog_nbytes = (data[7] << 8) | data[6];
-		prog_state = PROG_STATE_TPI_WRITE;
-		len = 0xff; /* multiple out */
-
-	} else if (data[1] == USBASP_FUNC_GETCAPABILITIES) {
-		replyBuffer[0] = USBASP_CAP_0_TPI;
-		replyBuffer[1] = 0;
-		replyBuffer[2] = 0;
-		replyBuffer[3] = 0;
-		len = 4;
-	}
-
-	usbMsgPtr = replyBuffer;
-
-	return len;
+    usbMsgPtr = replyBuffer;
+    return len;
 }
 
 uchar usbFunctionRead(uchar *data, uchar len) {
+    uchar i;
 
-	uchar i;
+    /* check if programmer is in correct read state */
+    if ((prog.state != PROG_STATE_READFLASH) && 
+        (prog.state != PROG_STATE_READEEPROM) && 
+        (prog.state != PROG_STATE_TPI_READ)) {
+        return 0xff;
+    }
 
-	/* check if programmer is in correct read state */
-	if ((prog_state != PROG_STATE_READFLASH) && (prog_state
-			!= PROG_STATE_READEEPROM) && (prog_state != PROG_STATE_TPI_READ)) {
-		return 0xff;
-	}
+    /* fill packet TPI mode */
+    if(prog.state == PROG_STATE_TPI_READ) {
+        tpi_read_block(prog.address, data, len);
+        prog.address += len;
+        return len;
+    }
 
-	/* fill packet TPI mode */
-	if(prog_state == PROG_STATE_TPI_READ)
-	{
-		tpi_read_block(prog_address, data, len);
-		prog_address += len;
-		return len;
-	}
+    /* fill packet ISP mode */
+    for (i = 0; i < len; i++) {
+        if (prog.state == PROG_STATE_READFLASH) {
+            data[i] = ispReadFlash(prog.address);
+        } else {
+            data[i] = ispReadEEPROM(prog.address);
+        }
+        prog.address++;
+    }
 
-	/* fill packet ISP mode */
-	for (i = 0; i < len; i++) {
-		if (prog_state == PROG_STATE_READFLASH) {
-			data[i] = ispReadFlash(prog_address);
-		} else {
-			data[i] = ispReadEEPROM(prog_address);
-		}
-		prog_address++;
-	}
+    /* last packet? */
+    if (len < 8) {
+        prog.state = PROG_STATE_IDLE;
+    }
 
-	/* last packet? */
-	if (len < 8) {
-		prog_state = PROG_STATE_IDLE;
-	}
-
-	return len;
+    return len;
 }
 
 uchar usbFunctionWrite(uchar *data, uchar len) {
+    uchar retVal = 0;
+    uchar i;
 
-	uchar retVal = 0;
-	uchar i;
+    /* check if programmer is in correct write state */
+    if ((prog.state != PROG_STATE_WRITEFLASH) && 
+        (prog.state != PROG_STATE_WRITEEEPROM) && 
+        (prog.state != PROG_STATE_TPI_WRITE)) {
+        return 0xff;
+    }
 
-	/* check if programmer is in correct write state */
-	if ((prog_state != PROG_STATE_WRITEFLASH) && (prog_state
-			!= PROG_STATE_WRITEEEPROM) && (prog_state != PROG_STATE_TPI_WRITE)) {
-		return 0xff;
-	}
+    if (prog.state == PROG_STATE_TPI_WRITE) {
+        tpi_write_block(prog.address, data, len);
+        prog.address += len;
+        prog.nbytes -= len;
+        if(prog.nbytes <= 0) {
+            prog.state = PROG_STATE_IDLE;
+            return 1;
+        }
+        return 0;
+    }
 
-	if (prog_state == PROG_STATE_TPI_WRITE)
-	{
-		tpi_write_block(prog_address, data, len);
-		prog_address += len;
-		prog_nbytes -= len;
-		if(prog_nbytes <= 0)
-		{
-			prog_state = PROG_STATE_IDLE;
-			return 1;
-		}
-		return 0;
-	}
+    for (i = 0; i < len; i++) {
+        if (prog.state == PROG_STATE_WRITEFLASH) {
+            /* Flash */
+            if (prog.pagesize == 0) {
+                /* not paged */
+                ispWriteFlash(prog.address, data[i], 1);
+            } else {
+                /* paged */
+                ispWriteFlash(prog.address, data[i], 0);
+                prog.pagecounter--;
+                if (prog.pagecounter == 0) {
+                    ispFlushPage(prog.address, data[i]);
+                    prog.pagecounter = prog.pagesize;
+                }
+            }
+        } else {
+            /* EEPROM */
+            ispWriteEEPROM(prog.address, data[i]);
+        }
 
-	for (i = 0; i < len; i++) {
+        prog.nbytes--;
 
-		if (prog_state == PROG_STATE_WRITEFLASH) {
-			/* Flash */
+        if (prog.nbytes == 0) {
+            prog.state = PROG_STATE_IDLE;
+            if ((prog.blockflags & PROG_BLOCKFLAG_LAST) && 
+                (prog.pagecounter != prog.pagesize)) {
+                /* last block and page flush pending, so flush it now */
+                ispFlushPage(prog.address, data[i]);
+            }
+            retVal = 1;
+        }
 
-			if (prog_pagesize == 0) {
-				/* not paged */
-				ispWriteFlash(prog_address, data[i], 1);
-			} else {
-				/* paged */
-				ispWriteFlash(prog_address, data[i], 0);
-				prog_pagecounter--;
-				if (prog_pagecounter == 0) {
-					ispFlushPage(prog_address, data[i]);
-					prog_pagecounter = prog_pagesize;
-				}
-			}
+        prog.address++;
+    }
 
-		} else {
-			/* EEPROM */
-			ispWriteEEPROM(prog_address, data[i]);
-		}
-
-		prog_nbytes--;
-
-		if (prog_nbytes == 0) {
-			prog_state = PROG_STATE_IDLE;
-			if ((prog_blockflags & PROG_BLOCKFLAG_LAST) && (prog_pagecounter
-					!= prog_pagesize)) {
-
-				/* last block and page flush pending, so flush it now */
-				ispFlushPage(prog_address, data[i]);
-			}
-
-			retVal = 1; // Need to return 1 when no more data is to be received
-		}
-
-		prog_address++;
-	}
-
-	return retVal;
+    return retVal;
 }
 
 void hardwareInit(void) {
@@ -419,4 +333,3 @@ int main(void) {
 
 	return 0;
 }
-
